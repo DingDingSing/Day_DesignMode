01e43494970659987611a8bb6c46163ee6dfe646
于生宇 <yushengyu@58.com>
2021-05-29 01:10:00 +0800

    feat: 删除重复节点

---
+++ week2/83.js
@@ -0,0 +1,30 @@
+// 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+
+// 返回同样按升序排列的结果链表。
+// https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  const node = new ListNode();
+  node.next = head;
+
+  while (head && head.next) {
+    if (head.val === head.next.val) {
+      head.next = head.next.next;
+    } else {
+      head = head.next;
+    }
+  }
+
+  return node.next;
+};
