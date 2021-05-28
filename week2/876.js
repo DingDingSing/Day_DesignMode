a1073af8d2a2cbcfe7dffa0d95c72f07b56465b0
于生宇 <yushengyu@58.com>
2021-05-29 00:43:37 +0800

    feat: 链表快慢指针

---
+++ week2/876.js
@@ -0,0 +1,27 @@
+// 给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+
+// 如果有两个中间结点，则返回第二个中间结点。
+
+// https://leetcode-cn.com/problems/middle-of-the-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+*/
+// 快慢指针
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
